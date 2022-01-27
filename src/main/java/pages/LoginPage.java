47a0d20dd0464208fb4d1dbaa2baa6277245ec47
sajad <sajadny@yahoo.com>
2022-01-26 22:32:42 -0500

    "Extra Line Of Code Added"

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -3,6 +3,7 @@ package pages;
 public class LoginPage {
     public void loginPage(){
         System.out.println("This is log in method");
+        System.out.println("This is new commit added ");
     }
 
 }
